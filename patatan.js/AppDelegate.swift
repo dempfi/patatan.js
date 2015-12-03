7bc970d16633f4d20397ed335bf6388ed5325f23
Ike Ku <dempfi@yahoo.com>
2015-12-03 13:59:49 +0300

    Initial Commit

---
+++ patatan.js/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  patatan.js
+//
+//  Created by Ike Ku on 12/3/15.
+//  Copyright © 2015 Ike Ku. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
